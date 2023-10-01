07bef30f9843707c27bf98cdb2f21179b7d5242e
kosar mirshahi <kosarmirshahi@gmail.com>
2023-10-01 15:56:18 +0330

    create components

---
+++ src/components/Form/Form.js
@@ -0,0 +1,12 @@
+import "./Form.css";
+function Form() {
+  return (
+    <div>
+      <p>Enter your item</p>
+      <input type="text" />
+      <button>Add</button>
+    </div>
+  );
+}
+
+export default Form;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import "./Header.css";
+function Header() {
+  return (
+    <div>
+      <h1>Travel Packing Checklist</h1>
+      <h4 className="destination">Destination</h4>
+      <h5>Iran Tehran</h5>
+    </div>
+  );
+}
+
+export default Header;
